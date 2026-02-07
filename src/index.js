c2430b6bee3312afe716241be8efae15cd34d80b
Your Name <you@example.com>
2026-02-07 17:38:18 +0530

    Setup basic body-parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const{ PORT } = require('./config/serverconfig');
 
 const setupandstartserver = () => {
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    
     app.listen(PORT,() =>{
         console.log(`Server started at ${PORT}`);
     })
