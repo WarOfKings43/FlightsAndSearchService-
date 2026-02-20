7d02c9b4ba5c77e4551c901219c6d4aa43a90b3a
Your Name <you@example.com>
2026-02-20 18:12:46 +0530

    Added city services

---
+++ src/services/city-service.js
@@ -0,0 +1,49 @@
+const {CityRepsitory} = require('../repository/index'); 
+
+class CityService{
+    constructor(){
+        this.cityRepository = new CityRepsitory();
+    }
+
+    async createCity(data){
+        try {
+            const city = await this.cityRepository.create(data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the service layer");
+            throw {error};
+        }
+    }
+    
+    async deleteCity(cityId){
+        try {
+            const response = await this.cityRepository.destroy(cityId);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in the service layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await this.cityRepository.updateCity(cityId, data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the service layer");
+            throw {error};
+        }
+    }
+    
+    async getCity(cityId){
+        try {
+            const city = await this.cityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the service layer");
+            throw {error};
+            }   
+    }
+} 
+
+module.exports = CityService;
